2ffe6a6b4af5f9743268cb0935fa1c0abaf330db
LILIAN HONORIO TEIXEIRA 114111080 <lilianht@lcc1-03.lcc.ufcg.edu.br>
2016-08-11 17:20:48 -0300

    Inicio do lab 6, aula dia 11 de agosto

--- src/Usuario/Noob.java
+++ src/Usuario/Noob.java
@@ -3,6 +3,10 @@ package Usuario;
 
 public class Noob extends Usuario {
 
+	private int desconto = 10; //10 % de desconto
+	private int x2p = 0;
+
+	
 	public Noob(String nome, String nomeLogin, double dinheiro) {
 		super(nome, nomeLogin, dinheiro);
 		// TODO Auto-generated constructor stub
--- src/Usuario/Veterano.java
+++ src/Usuario/Veterano.java
@@ -3,11 +3,13 @@ package Usuario;
 
 public class Veterano extends Usuario{
 
+	private int desconto = 20; //20 % de desconto
+	private int x2p = 1000; //a cada compra de jogo ganha x2p
+	
 	public Veterano(String nome, String nomeLogin, double dinheiro) {
 		super(nome, nomeLogin, dinheiro);
 		// TODO Auto-generated constructor stub
 	}
 
-	
 	
 }
